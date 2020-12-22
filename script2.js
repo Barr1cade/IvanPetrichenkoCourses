5cb997aa69967b2ef924b2aed7ed06ebf773202d
Nikolay <kolyakotik.vbg@gmail.com>
2020-12-22 20:21:26 +0300

    second homework

--- script2.js
+++ script2.js
@@ -1,16 +1,20 @@
 'use strict';
-let number = 26;
-// (number === 25) ? console.log('correct!'): console.log('wrong!');
-switch (number) {
-    case 30:
-        console.log('too much!');
-        break;
-    case 15:
-        console.log('not enough');
-        break;
-    case 25:
-        console.log('got it!');
-        break;
-    default:
-        console.log('next time stranger!');
+// let number = 26;
+// // (number === 25) ? console.log('correct!'): console.log('wrong!');
+// switch (number) {
+//     case 30:
+//         console.log('too much!');
+//         break;
+//     case 15:
+//         console.log('not enough');
+//         break;
+//     case 25:
+//         console.log('got it!');
+//         break;
+//     default:
+//         console.log('next time stranger!');
+// }
+
+for (let i = 1; i < 0; i++) {
+    console.log('error');
 }
