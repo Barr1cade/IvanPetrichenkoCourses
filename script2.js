2eb27989878f5376852d06215464bc36065ea62f
Nikolay <kolyakotik.vbg@gmail.com>
2020-12-22 16:29:03 +0300

    cycles lesson

---
+++ script2.js
@@ -0,0 +1,16 @@
+'use strict';
+let number = 26;
+// (number === 25) ? console.log('correct!'): console.log('wrong!');
+switch (number) {
+    case 30:
+        console.log('too much!');
+        break;
+    case 15:
+        console.log('not enough');
+        break;
+    case 25:
+        console.log('got it!');
+        break;
+    default:
+        console.log('next time stranger!');
+}
